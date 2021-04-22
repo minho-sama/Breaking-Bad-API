e4d4b0ab5909162cd60a34da49ef690a3691d661
minho-sama <n.a.minh1106@gmail.com>
2021-04-23 01:14:35 +0200

    main functionality ready, some features are still missing

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
-//TODO: catch: hibauzenet couldnt find character, quote, opacity, enter a tring
+//todo: RANDOMBUTTON, enter a name pls ha üres a mező --> piros keret
+//hibauzenet couldnt find character --> odakúrni egy képet meg a feliratot, előtte content.innerHTML = ""
+//majd error handling: something went wrong: we couldnt find the character youre looking for/check your internet connection
+
+    //landing page
+window.addEventListener('load', () => {
+    getCharByName('Walter White')
+        .then(data => renderCharacter(data.img, data.name, data.occupation, data.nickname, data.appearance, data.portrayed, getCharQuote(data.name)))
+        .finally(() =>{
+            document.querySelector('.content').style.opacity = 1;
+        })
+})
 
 const search = document.querySelector('.fa-search');
 search.addEventListener('click', () =>{
     getCharByName()
         .then(data => {
-            renderCharacter(data.img, data.name, data.occupation, data.nickname, data.appearance, data.portrayed, 'idézet idézet idézet')
+            renderCharacter(data.img, data.name, data.occupation, data.nickname, data.appearance, data.portrayed, getCharQuote(data.name));
         })
         .finally( () => {
-            document.querySelector('.container').style.opacity = 1
+            document.querySelector('.content').style.opacity = 1;
         });
 })
-
-//majd error handling: something went wrong: we couldnt find the character youre looking for
 
 const getCharByName = async function (name = getUSerInput()){
     try{
@@ -26,20 +35,43 @@ const getCharByName = async function (name = getUSerInput()){
         console.log(err);
     };
 };
-document.querySelector('#random-btn').addEventListener('click', () => getCharByName())
-// getCharByName('walter').then(data => console.log(data.name));
 
+function getCharQuote (name){
+        console.log(name.split(" "));
+        let firstName, lastName;
+        [firstName, lastName] = name.split(" ");
+
+        return fetch(`https://www.breakingbadapi.com/api/quote/random?author=${firstName}+${lastName}`)
+                    .then(res => {
+                        console.log(res)
+                        if (!res.ok) throw new Error('404 no quote found');
+                        else{
+                            return res.json()
+                        }
+
+                    })
+                    .then(data => {
+                        console.log(data)
+
+                        const [quoteData] = data;
+
+                        console.log(quoteData.quote);
+
+                        return quoteData.quote
+                    })
+                    .catch(err => console.log(err))
+    }
 
 function getUSerInput(){
     const search = document.querySelector('#searchbox')
-    if (search.value === "") return alert('error message: pls enter a name') //itt egy keretformázás lesz setTimeouttal h töltse ki pls a mezőt
+    if (search.value === "") return alert('error message: pls enter a name'); //itt egy keretformázás lesz setTimeouttal h töltse ki pls a mezőt
     return search.value
 }
 
-function renderCharacter (img, name, occ, nick, seasons, port, quote){
-    clearContent()
-    let card = document.createElement('div')
-    card.classList.add('container')
+async function renderCharacter (img, name, occ, nick, seasons, port, quote){
+    clearContent();
+    let card = document.createElement('div');
+    card.classList.add('container');
     card.innerHTML = `
     <img class = "profilePic" src = "${img}">
     <div class = "info">
@@ -54,22 +86,23 @@ function renderCharacter (img, name, occ, nick, seasons, port, quote){
         </div>
         <div class = "quotes">
             <ul class = "list">
-                <li>${quote}</li>
+                <li id = "quoteLine">,,${await quote}''</li>
             </ul>
         </div>
     </div>
     `
-    document.querySelector('.content').appendChild(card)
+    document.querySelector('.content').appendChild(card);
+    //"error handling"
+    const [firstName] = name.split(" ")
+    const quoteValue = document.querySelector('#quoteLine');
+    if (quoteValue.textContent === ",,undefined''") {
+        quoteValue.textContent = `sorry, we couldn't find a quote for ${firstName}`
+    };
 
-    //opacity RESOLVEDBA kell
 }
-// renderCharacter("https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/el-camino-breaking-bad-walter-white-1570791198.jpg?crop=0.420xw:1.00xh;0.137xw,0&resize=480:*",
-// 'walter', "criminal", 'walt', '1-5', 'bryan', 'i am the danger')
-
 
 function clearContent(){
+    document.querySelector('#searchbox').value = ""
+    document.querySelector('.content').style.opacity = 0;
     document.querySelector('.content').innerHTML = ""
 }
-
-
-
